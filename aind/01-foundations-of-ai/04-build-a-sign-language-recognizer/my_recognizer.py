39604c2f83b00cbd477d0051ad6be1a8dc5f8216
Thomas Dang <thomas@lingomaniacs.com>
2018-02-12 03:19:11 +0100

    Add build-a-sign-language-recognizer project to foundations-of-ai

---
+++ aind/01-foundations-of-ai/04-build-a-sign-language-recognizer/my_recognizer.py
@@ -0,0 +1,46 @@
+import warnings
+from asl_data import SinglesData
+
+
+def recognize(models: dict, test_set: SinglesData):
+    """ Recognize test word sequences from word models set
+
+   :param models: dict of trained models
+       {'SOMEWORD': GaussianHMM model object, 'SOMEOTHERWORD': GaussianHMM model object, ...}
+   :param test_set: SinglesData object
+   :return: (list, list)  as probabilities, guesses
+       both lists are ordered by the test set word_id
+       probabilities is a list of dictionaries where each key a word and value is Log Liklihood
+           [{SOMEWORD': LogLvalue, 'SOMEOTHERWORD' LogLvalue, ... },
+            {SOMEWORD': LogLvalue, 'SOMEOTHERWORD' LogLvalue, ... },
+            ]
+       guesses is a list of the best guess words ordered by the test set word_id
+           ['WORDGUESS0', 'WORDGUESS1', 'WORDGUESS2',...]
+   """
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
+    probabilities = []
+    guesses = []
+    
+    # TODO implement the recognizer
+    X_lengths = test_set.get_all_Xlengths()
+    for X, lengths in X_lengths.values():
+        log_l = {}                             # save the likelihood of a word
+        max_score = float("-inf")              # save max score as recognizer iterates the list
+        best_guess = None                      # save best guess as recognizer iterates the list
+        for word, model in models.items():
+            try:
+                # score word using model
+                word_score = model.score(X, lengths)
+                log_l[word] = word_score
+
+                if word_score > max_score:
+                    max_score = word_score
+                    best_guess = word
+            except:
+                # unable to process word
+                log_l[word] = float("-inf")
+
+        guesses.append(best_guess)
+        probabilities.append(log_l)
+
+    return probabilities, guesses
